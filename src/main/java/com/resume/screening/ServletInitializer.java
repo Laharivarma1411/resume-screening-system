b60626b4628aa3b5da86af55dd2a6451fafd70f5
PAKALAPATI LAHARI <p.lahari2022@gmail.com>
2026-05-29 12:05:50 +0530

    Add files via upload

---
+++ src/main/java/com/resume/screening/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.resume.screening;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ScreeningApplication.class);
+	}
+
+}
---
+++ src/test/java/com/resume/screening/ScreeningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resume.screening;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreeningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
